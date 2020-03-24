6bd016f31eaf489b4cfcfdafb650967acb76a730
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2020-03-24 18:21:46 -0500

    implemented sklearn.naive_bayes.GaussianNB
    - imported some sklearn.naive_bayes classifiers and found out that they didn't work well at all with random numbers, they probably don't follow a normal distribution

--- regression.py
+++ regression.py
@@ -6,7 +6,7 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-n_samples, n_features = 6, 2
+n_samples, n_features = 20, 5
 rng = np.random.RandomState(0) #something to give random numbers
 y = rng.randn(n_samples) #an array with 10 random samples between 0 and 1
 x = rng.randn(n_samples, n_features) #a 2D array with 10 subarrays (features) with 5 rand floats each
@@ -51,5 +51,21 @@ clf.fit(x_train,y_train)
 GDA = LinearDiscriminantAnalysis()
 x_train, x_test, y_train, y_test = train_test_split(x,y,random_state=42)
 GDA.fit(x,y)
-print('test results: ',GDA.predict(x_test))
-print("actual: ",y_test)
+#print('test results: ',GDA.predict(x_test))
+#print("actual: ",y_test)
+
+
+#Naive Bayes implementations, which use the assumption that (by the chain rule of probability,
+#P(x_2 | y,x_1)) = P(x_2 | y): that is, knowing other variables should not determine P(x_n | y)
+#it is a 'naive' assumption, but its mostly accurate for things like text classification
+from sklearn.naive_bayes import GaussianNB
+gaussNBClassifier = GaussianNB()
+gaussNBClassifier.fit(x_train,y_train)
+#print("test results: ",gaussNBClassifier.predict(x_test))
+#print("actual: ",y_test)  #gaussNB isn't *too* accurate, random nums may not follow the distribution needed aka be gaussian haw
+
+from sklearn.naive_bayes import BernoulliNB
+bernNB = BernoulliNB(binarize=True)
+bernNB.fit(x_train,y_train)
+#print("test results: ",bernNB.predict(x_test))
+#print("actual: ",y_test)  #not accurate at all, random data probably isn't meant for bernoulli distributions
