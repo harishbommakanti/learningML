599761e25e55c08cc6e51ca924c7687d56759f41
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2020-03-25 18:42:43 -0500

    added some matplotlib.pyplot examples to the file

--- dataVisualization.py
+++ dataVisualization.py
@@ -1,2 +1,23 @@
-import matplotlib
-#show the distribution of like 1000 rand vars
+import matplotlib.pyplot as plt
+import numpy as np
+
+randomNumGen = np.random
+
+#simple distribution of like 500 rand vars between 1 and 1000
+x = randomNumGen.randint(1000,size=500)
+#plt.plot(x,'ro')
+#plt.show()
+
+#showing f(x) v x for simple functions
+def f(x):
+    #f(x) = x^2
+    #return x**2
+
+    #f(x) = bell curve
+    sigma=1
+    mu = 0
+    return (1 / 2*np.pi*np.sqrt(sigma)) * np.exp(- (x - mu)**2 / 2*(sigma**2))
+
+x = randomNumGen.uniform(-5,5,[1,5000])
+plt.plot(x,f(x),'ro')
+plt.show()
