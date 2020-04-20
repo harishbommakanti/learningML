d069583335a7e98ab63be661011e9591f86a5df4
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2020-04-20 17:06:25 -0500

    added code for cross validation based off of lecture notes

--- CNN_coding/nearest_neighbors.py
+++ CNN_coding/nearest_neighbors.py
@@ -47,3 +47,27 @@ def runVanillaNearestNeighbors():
     print('accuracy: %f' % ( np.mean(Yte_predict == Yte)))
 
 #runVanillaNearestNeighbors()
+
+def cross_validation():
+    Xtr_rows,Ytr_rows = [],[]
+    Xtr,Ytr = [],[]
+    # assume we have Xtr_rows, Ytr, Xte_rows, Yte as before
+    # recall Xtr_rows is 50,000 x 3072 matrix
+    Xval_rows = Xtr_rows[:1000, :] # take first 1000 for validation
+    Yval = Ytr[:1000]
+    Xtr_rows = Xtr_rows[1000:, :] # keep last 49,000 for train
+    Ytr = Ytr[1000:]
+
+    # find hyperparameters that work best on the validation set
+    validation_accuracies = []
+    for k in [1, 3, 5, 10, 20, 50, 100]: #try to find the optimal k
+        # use a particular value of k and evaluation on validation data
+        nn = NearestNeighbor()
+        nn.train(Xtr_rows, Ytr)
+        # here we assume a modified NearestNeighbor class that can take a k as input
+        Yval_predict = nn.predict(Xval_rows, k = k)
+        acc = np.mean(Yval_predict == Yval) # (number where predictions are correct)/total #
+        print('accuracy: %f' % (acc,))
+
+        # keep track of what works on the validation set
+        validation_accuracies.append((k, acc))
