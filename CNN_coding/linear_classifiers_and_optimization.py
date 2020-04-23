9a50e71bf02809a4027beb0e82559b008c8685c6
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2020-04-23 15:05:06 -0500

    added a test function to test the SVM and softmax losses, they work

--- CNN_coding/linear_classifiers_and_optimization.py
+++ CNN_coding/linear_classifiers_and_optimization.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-#the loss of each class given input x, output y, prediction weights W
+#the loss of each class given input x, correct class label index y, prediction weights W
 #for the multi class SVM loss function
 def SVM_L_i_vectorized(x,y,W):
 	scores = W.dot(x) #predictions for each image
@@ -10,11 +10,23 @@ def SVM_L_i_vectorized(x,y,W):
 	return loss_i
 
 
-#used more widely, this performs softmax loss on input x, output y,
-#prediction weights W, and a class label index i to return the loss for a certain class
-def Softmax_L_vectorized(x,y,W,i):
+#used more widely, this performs softmax loss on input x, correct class label index y,
+#prediction weights W
+def Softmax_L_vectorized(x,y,W):
     scores = W.dot(x) #classic example of a score function
     scores = np.exp(scores) #exponentiate each term
     scores = scores/np.sum(scores) #normalize each term
     L = -np.log(scores) #take the log
-    return L[i]
+    return L[y]
+
+def test_loss():
+    x = np.random.randn(5,1) #5 total inputs
+    W = np.random.randn(3,5) #output is W.dot(x) -> 3 x 1 matrix
+    y = 2 #index to query the loss of
+    print("x: ",x)
+    print("W: ",W)
+    print("y: ",y)
+    print("SVM loss: ",SVM_L_i_vectorized(x,y,W))
+    print("Softmax loss: ",Softmax_L_vectorized(x,y,W)[0])
+
+test_loss()
