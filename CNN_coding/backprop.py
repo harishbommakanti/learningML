b93db8b799260bf665e9329d28787df191426bb3
Harish Bommakanti <33380236+harishbommakanti@users.noreply.github.com>
2020-04-24 11:36:30 -0500

    added more complicated examples of backprop, such as using lots of quotient rules and matrix multiplication

--- CNN_coding/backprop.py
+++ CNN_coding/backprop.py
@@ -32,3 +32,68 @@ def sigmoid_chain_rule_ex():
     dx = [w[0] * ddot, w[1] * ddot] # backprop into x
     dw = [x[0] * ddot, x[1] * ddot, 1.0 * ddot] # backprop into w
     # we're done! we have the gradients on the inputs to the circuit
+
+def complex_backprop_example():
+    #sigma(x) = 1/1+e^(-x)
+    #f(x,y) = (x + sigma(y)) / ((sigma(x) + (x+y)^2)
+    
+    x = 3 # example values
+    y = -4
+
+    # forward pass
+    sigy = 1.0 / (1 + np.exp(-y)) # sigmoid in numerator   #(1)
+    num = x + sigy # numerator                               #(2)
+    sigx = 1.0 / (1 + np.exp(-x)) # sigmoid in denominator #(3)
+    xpy = x + y                                              #(4)
+    xpysqr = xpy**2                                          #(5)
+    den = sigx + xpysqr # denominator                        #(6)
+    invden = 1.0 / den                                       #(7)
+    f = num * invden # done!                                 #(8)
+
+
+    #backward pass
+    #backprop of f = num * invden: gradient switcher
+    dnum = invden
+    dinvden = num
+
+    #backprop of invden = 1.0/den: local gradient of 1/den = -1/(den^2), multiply that by df/d(invden)
+    dden = (-1.0 / (den**2)) * dinvden
+
+    #backprop of den = sigx + xpysqr: pass in 1 cuz +, end up with 1 * df/d(den)
+    dsigx = 1 * dden
+    dxpysqr = 1 * dden
+
+    #backprop of dxpysqr = xpy**2, local gradient is 2*xpy, prev gradient is df/d(xpysqr)
+    dxpy = (2 * dxpy) * dxpysqr
+
+    #backprop of xpy = dx + dy: passess 1 as a gradient cuz +, 1 + df/d(xpy)
+    dx = 1*dxpy
+    dy = 1*dxpy
+
+    #backprop of sigx = 1/(1+math.exp(-x)): quotient rule
+    dx += ((1 - sigx)*sigx) * dsigx # += as gradients add at branching
+
+    #backprop of num = x + sigy: df/dx += 1 * dnum (+ gate), df/dsigy += 1 * dsigy
+    dx += 1*dnum
+    dsigy = 1*dnum
+
+    #backprop of sigy = 1/(1+matht.exp(-y))
+    dy += ((1-sigy)*sigy)*dsigy
+
+    #go back and put df in front of every d_ for this to make sense, simple df/dx = df/d_ * d_/dx
+
+def matrix_multiplication_backprop_ex():
+    #gradients for multiplying matrices
+
+    #forward pass (simple loss computation)
+    W = np.random.randn(5,10) # a 5x10 matrix
+    X = np.random.randn(10,3) # a 10x3 matrix
+    D = W.dot(X) # a 5x3 matrix
+
+    # backprop
+    dD = np.random.randn(*D.shape)
+    dW = dD.dot(X.T) #5x3 * 3x10 --> 5x10, shape of W. dW has same shape as W, good
+    dX = W.T.dot(dD) #10x5 * 5x3 --> 10x3, shape of X. dX has same shape as X, good
+    #its the same type of thing for multiplication regularly, gradient switching
+
+    #USE DIMENSION ANALYSIS: check if d_ has same shape as _, the Jacobian should
