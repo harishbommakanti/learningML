7d1ce543da7c97c2488e0b67ac4a1f80749d6605
Harish <harishbommakanti2002@gmail.com>
2020-07-07 11:38:42 -0500

    added lecture 7 CNN231N notes about fancier methods of optimization; added a .gitignore

--- CNN_coding/linear_classifiers_and_optimization.py
+++ CNN_coding/linear_classifiers_and_optimization.py
@@ -81,3 +81,91 @@ def eval_numerical_gradient(f, x):
         it.iternext() # step to next dimension
 
     return grad
+
+#leave blank for now, just proof of concept
+def compute_gradient(x):
+    return 0
+
+#Fancier methods of gradient descent below, they don't get stuck at local optima etc.
+def SGD_and_Momentum(x):
+    #the idea is to have a running 'velocity' to jump over local optima
+    vx = 0
+    rho = 1e-2 #hyperparameter
+    learning_rate = 1e-3 #hyperparameter
+
+    while True:
+        dx = compute_gradient(x)
+        vx = rho * vx + dx
+        x += learning_rate * vx
+
+
+#advantage over regular momentum update is that it
+#gives an idea of the local landscape, aka gradient at new x instead of old x
+def Nesterov(x):
+    rho = 1e-2 #hyperparameter
+    learning_rate = 1e-3 #hyperparameter
+    v = 0
+
+    while True:
+        dx = compute_gradient(x)
+        old_v = v
+        v = rho * v - learning_rate * dx #look forward
+        x += -rho * old_v + (1 + rho) * v #update parameter vector, weighted diff between next velocity and curr velocity
+
+
+#another optimization method, not too common: keeps running sum of grad_squared
+def Adagrad(x):
+    grad_squared = 0
+    learning_rate = 1e-3 #hyperparameter
+
+    while True:
+        dx = compute_gradient(x)
+        grad_squared += dx*dx #keep running sum of square gradients
+
+        x -= learning_rate * dx / np.sqrt(grad_squared) + 1E-7 #helps to accelerate movement along slow dimensions, slow down movement along 'height of taco shells'
+    
+    #however as time goes on grad_square increases over time --> bad for NNs
+
+
+#variation of AdaGrad where it adresses problem of slowing down where you might not want too
+def RMSProp(x):
+    grad_squared = 0
+    decay_rate = 1e-2 #hyperparameter
+    learning_rate = 1e-3 #hyperparameter
+
+    while True:
+        dx = compute_gradient(x)
+        grad_squared = decay_rate * grad_squared + (1 - decay_rate) * dx * dx
+        x -= learning_rate * dx / (np.sqrt(grad_squared) + 1e-7)
+            #1e-7 cuz you never want to divide by 0
+
+
+#Instructor's favorite, 'default choice' for gradient descent
+def Adam(x):
+    #a mix of Momentum, bias correction, and RMSProp
+
+    first_moment = 0
+    second_moment = 0
+    num_iterations = 1000
+    learning_rate = 1e-4 #hyperparameter
+
+    #hyperparameters which have their ideal implementations below
+    beta1 = 0.9
+    beta2 = 0.99
+    alpha = 1e-3 #or 5e-4
+
+    for t in range(num_iterations):
+        dx = compute_gradient(x)
+
+        #MOMENTUM STEP BELOW
+        first_moment = beta1 * first_moment + (1-beta1) * dx
+
+        #RMSPROP step 1
+        second_moment = beta2 * second_moment + (1-beta2) * dx * dx
+
+        #Bias correction below
+        first_unbias = first_moment / (1 - beta1 ** t)
+        second_unbias = second_moment / (1 - beta2 ** t)
+
+        #Last step of RMSProp
+        x -= learning_rate * first_unbias / (np.sqrt(second_unbias) + 1e-7)
